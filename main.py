82375db9f96340788360b4e2dfbb0722fa081bb8
Maximilian Georg von Consbruch <m.v.consbruch@gmail.com>
2020-11-05 11:39:40 +0000

    relative paths now work properly.

--- main.py
+++ main.py
@@ -4,24 +4,44 @@ import requests as rq
 import os
 
 
-DIR = "./downloads/"
+# DIR specifies where downloaded files should be stored.
+DIR = "./download/"
 
 try: 
     os.mkdir(DIR)
-    print("creating " + DIR + "...")
+    print("creating " + DIR)
 except FileExistsError:
     pass
 
+# DFILE contains the past of .dfile, which keeps track of already-downloaded files.
 DFILE = DIR+"/.dfile"
-URL = "google.com"
 
+# URL of the website that is to be scraped.
+URL = "https://www.math.uni-bonn.de/people/gmartin/AlgebraicGeometryWS2020.html"
+
+# URL for relative paths.
+PATHURL = "URL"
+if not URL.endswith("/"):
+    PATHURL = ""
+    sp = URL.split("/")[:-1]
+    for s in sp:
+        PATHURL += s + "/"
+
+print(PATHURL)
+
+
+# TYPELIST is the list of desired filetypes.
 TYPELIST = ["pdf", "djvu"]
 
+
+
+### Algorithm starts here ###
 
 # Check if .dfile exists
 if not os.path.exists(DFILE):
     open(DFILE, 'a+').close()
     os.utime(DFILE, None)
+
 
 # load content of .dfile into dlist
 dirfilestream = open(DFILE, 'r')
@@ -35,14 +55,17 @@ r = rq.get(URL)
 source = r.text # source is a string, containing the source code of URL.
 source_split = source.split("\"")[1:]
 candidates = [sp.split("\"")[0] for sp in source_split] # candidates contains everything within quotes
-#print(candidates)
 
-found_files = [] # list to contain all files found on website
-for c in candidates: # check which candidates end in a type in TYPELIST
+
+# found_files will contain all files referenced on by website
+found_files = [] 
+
+# filter for canditates with correct file type
+for c in candidates: 
     if "//" in c:
         continue
 
-    flag = False
+    flag = False  # flag to check filetypes
     for TYPE in TYPELIST:
         if c.endswith(TYPE):
             flag = True
@@ -51,22 +74,27 @@ for c in candidates: # check which candidates end in a type in TYPELIST
     if flag:
         found_files += [c]
 
-for f in found_files: # Download and store files
-    if f not in dlist:
-        print("Downloading " + f + "...")
+# Download files
+for f in found_files: 
+    # At this point, elements in found_files might still contain '/'s. this is bad. fname removes those.
+    fname = f.split("/")[-1] 
 
-        r = rq.get(URL + f)
+    if fname not in dlist:
+        print("Downloading " + fname + "...")
 
-        # At this point, elements in found_files might still contain '/'s. this is bad. 
-        fname = f.split("/")[-1] 
+        r = rq.get(PATHURL + f)
 
-        stream = open(DIR+fname, 'wb').write(r.content)
-        stream.close()
+        # write content to file
+        s = open(DIR+fname, 'wb')
+        s.write(r.content)
+        s.close()
 
+        # append to .dfile
         dfstream = open(DFILE, 'a+')
-        dfstream.write(f + '\n')
+        dfstream.write(fname + '\n')
         dfstream.close()
+
     else:
-        print(f + " already exists!")
+        print(fname + " already exists!")
 
 
